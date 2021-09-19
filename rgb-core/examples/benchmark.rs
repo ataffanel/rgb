8fda6891a882713718f8b0459901d253388a1e65
Arnaud Taffanel <arnaud@bitcraze.io>
2021-09-19 09:53:45 +0200

    Add small benchmark example

---
+++ rgb-core/examples/benchmark.rs
@@ -0,0 +1,35 @@
+use std::process::exit;
+
+
+fn main(){
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        println!("Usage: {} <rom>", args[0]);
+        exit(-1);
+    }
+
+    let cart = rgb_core::cart::Cart::load(&args[1], None).unwrap();
+    let mut cpu = rgb_core::cpu::Cpu::new(rgb_core::bootstrap::Bootstrap::create_default(), cart);
+
+
+    let cycles = 40_000_000;
+    let start = std::time::Instant::now();
+    let mut frames = 0;
+
+    for _ in 0..cycles {
+        cpu.step();
+        cpu.mem.step();
+        cpu.mem.reg_if |= cpu.mem.timer.step(cpu.cycle);
+        cpu.mem.reg_if |= cpu.mem.video.step(cpu.cycle);
+        cpu.mem.joypad.step();
+
+        if cpu.mem.video.image_ready {
+            frames += 1;
+        }
+    }
+
+    let runtime = std::time::Instant::now() - start;
+
+    println!("Runtime: {:?}, fps: {}", runtime, (frames as f64) / runtime.as_secs_f64() );
+}
